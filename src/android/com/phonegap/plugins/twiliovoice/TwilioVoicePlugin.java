af7d34f9ceff74019ca6b38e3af0b6f1949d11fa
Paulina Gallo <paulinagallo@webjunto.com>
2017-10-28 14:23:22 -0400

    inject this

--- src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
+++ src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
@@ -189,12 +189,12 @@ public class TwilioVoicePlugin extends CordovaPlugin {
 			}
 
 			if(NoiseSupressor.isAvailable()) {
-				AlertDialog dialog = new AlertDialog();
-				alertDialog.show();
+				AlertDialog dialog = new AlertDialog(this);
+				dialog.show();
 			}
 
 			if(AcousticEchoCanceler.isAvailable()) {
-				AlertDialog dialog = new AlertDialog();
+				AlertDialog dialog = new AlertDialog(this);
 				dialog.show();
 			}
 
