a886cf236ad1ac71da1c9301261f7e0ac9bba5ec
Paulina Gallo <paulinagallo@webjunto.com>
2017-10-28 16:35:07 -0400

    Adding setAudioFocus

--- src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
+++ src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
@@ -5,15 +5,30 @@ import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.DialogInterface;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -640,12 +655,14 @@ public class TwilioVoicePlugin extends CordovaPlugin {
 	private Call.Listener mCallListener = new Call.Listener() {
 		@Override
 		public void onConnectFailure(Call call, CallException exception) {
+			setAudioFocus(false);
 			mCall = null;
 			javascriptErrorback(exception.getErrorCode(), exception.getMessage(), mInitCallbackContext);
 		}
 
 		@Override
 		public void onConnected(Call call) {
+			setAudioFocus(true);
 			mCall = call;
 
 			JSONObject callProperties = new JSONObject();
@@ -664,10 +681,50 @@ public class TwilioVoicePlugin extends CordovaPlugin {
 
 		@Override
 		public void onDisconnected(Call call, CallException error) {
+			setAudioFocus(false);
 			mCall = null;
 			javascriptCallback("oncalldiddisconnect",mInitCallbackContext);
 		}
 	};
+
+	private void setAudioFocus(boolean setFocus) {
+        if (audioManager != null) {
+            if (setFocus) {
+                savedAudioMode = audioManager.getMode();
+                // Request audio focus before making any device switch.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    AudioAttributes playbackAttributes = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                            .build();
+                    AudioFocusRequest focusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+                            .setAudioAttributes(playbackAttributes)
+                            .setAcceptsDelayedFocusGain(true)
+                            .setOnAudioFocusChangeListener(new AudioManager.OnAudioFocusChangeListener() {
+                                @Override
+                                public void onAudioFocusChange(int i) { }
+                            })
+                            .build();
+                    audioManager.requestAudioFocus(focusRequest);
+                } else {
+                    audioManager.requestAudioFocus(null, AudioManager.STREAM_VOICE_CALL,
+                            AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                }
+                /*
+                 * Start by setting MODE_IN_COMMUNICATION as default audio mode. It is
+                 * required to be in this mode when playout and/or recording starts for
+                 * best possible VoIP performance. Some devices have difficulties with speaker mode
+                 * if this is not set.
+                 */
+                audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
+            } else {
+                audioManager.setMode(savedAudioMode);
+                audioManager.abandonAudioFocus(null);
+            }
+        }
+    }
+
+
 
 	private String getCallState(CallState callState) {
 		if (callState == CallState.CONNECTED) {
